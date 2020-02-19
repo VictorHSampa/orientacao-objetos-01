8c49deb25734799beb9a04b954a4484ea8462b7d
VictorHSampa <victorhugosampa@gmail.com>
2020-02-19 20:46:03 -0300

    Criação de classe Retangulo e RetanguloTeste

---
+++ src/test/java/exercicios/RetanguloTeste.java
@@ -0,0 +1,45 @@
+package exercicios;
+
+import exemplos.Quadrado;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class RetanguloTeste {
+    @Test
+    public void criarObjetoRetangulo(){
+        Retangulo r1 = new Retangulo();
+        assertNotNull(r1);
+    }
+
+    @Test
+    public void presencaAtributoLado(){
+        Retangulo r1 = new Retangulo();
+        r1.base = 10.0;
+        r1.altura = 5.0;
+        assertNotNull(r1.base);
+        assertNotNull(r1.altura);
+    }
+
+    @Test
+    public void metodoCalcularArea() {
+        Retangulo r1 = new Retangulo();
+        r1.base = 10.0;
+        r1.altura = 5.0;
+        Double valorEsperado = 50.0;
+        Double valorObtido = r1.calcularArea();
+        assertEquals(valorEsperado, valorObtido);
+    }
+
+    @Test
+    public void metodoCalcularPerimetro() {
+        Retangulo r1 = new Retangulo();
+        r1.base = 10.0;
+        r1.altura = 5.0;
+        Double valorEsperado = 30.0;
+        Double valorObtido = r1.calcularPerimetro();
+        assertEquals(valorEsperado, valorObtido);
+    }
+}
+}
